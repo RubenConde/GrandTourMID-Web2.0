a427067398c7e620c8759db166241882d6162ad0
Rubén de Jesús Conde Magaña <rubenconde_mag96@hotmail.com>
2018-04-03 19:17:31 -0500

    agregar preguntas

--- GrandTourMID/GrandTourMID/DAO/PreguntaDAO.cs
+++ GrandTourMID/GrandTourMID/DAO/PreguntaDAO.cs
@@ -14,17 +14,13 @@ namespace GrandTourMID.DAO
 
         public int AgregarPregunta(PreguntasBO objep)
         {
-            cmd = new SqlCommand("insert into pregunta (pregunta, idlugar) output inserted.idpregunta values (@pregunta, @idlugar)");
+            cmd = new SqlCommand("insert into Respuesta (textorespuesta, pregunta, textoincorrecta, textoincorrecta2, textoincorrecta3, idlugar)values(@textorespuesta, @pregunta, @textoincorrecta, @textoincorrecta2, @textoincorrecta3, @idlugar)");
+            cmd.Parameters.Add("@textorespuesta", SqlDbType.VarChar).Value = objep.correcta;
             cmd.Parameters.Add("@pregunta", SqlDbType.VarChar).Value = objep.pregunta;
+            cmd.Parameters.Add("@textoincorrecta", SqlDbType.VarChar).Value = objep.respuesta2;
+            cmd.Parameters.Add("@textoincorrecta2", SqlDbType.VarChar).Value = objep.respuesta3;
+            cmd.Parameters.Add("@textoincorrecta3", SqlDbType.VarChar).Value = objep.respuesta4;
             cmd.Parameters.Add("@idlugar", SqlDbType.Int).Value = objep.idlugar;
-            cmd.CommandType = CommandType.Text;
-            int idpregunta = EjecutarComando(cmd);
-            cmd = new SqlCommand("insert into Respuestas (correcta, respuesta2, respuesta3, respuesta4, idpregunta)values(@correcta, @re2, @re3, @re4, @id)");
-            cmd.Parameters.Add("@correcta", SqlDbType.VarChar).Value = objep.correcta;
-            cmd.Parameters.Add("@re2", SqlDbType.VarChar).Value = objep.respuesta2;
-            cmd.Parameters.Add("@re3", SqlDbType.VarChar).Value = objep.respuesta3;
-            cmd.Parameters.Add("@re4", SqlDbType.VarChar).Value = objep.respuesta4;
-            cmd.Parameters.Add("@id", SqlDbType.VarChar).Value = idpregunta;
             cmd.CommandType = CommandType.Text;
             return EjecutarComando(cmd);
         }
